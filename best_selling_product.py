a3e5757be1134b0a44550aa6e25d3b06b6f26695
sheikha22 <25f27109@mec.edu.om>
2026-01-05 12:16:14 +0400

    Initial version of best-selling product algorithm

---
+++ best_selling_product.py
@@ -0,0 +1,17 @@
+salesData = [
+    {"product": "A", "sales": 50},
+    {"product": "B", "sales": 80},
+    {"product": "C", "sales": 30}
+]
+
+maxSales = 0
+bestProduct = ""
+
+for product in salesData:
+    if product["sales"] > maxSales:
+        maxSales = product["sales"]
+        bestProduct = product["product"]
+    else:
+        bestProduct = bestProduct
+
+print("Best selling product:", bestProduct)
